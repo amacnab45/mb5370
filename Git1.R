1472cffc43de6e933f09d7c5337ee4b6768357b3
amacnab45 <alexander.macnab@my.jcu.edu.au>
2026-09-22 11:33:40 +1000

    secondcommit

---
+++ Git1.R
@@ -0,0 +1,6 @@
+## install if needed (do this exactly once):
+## install.packages("usethis")
+install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "amacnab45", user.email = "alexander.macnab@my.jcu.edu.au")
+
