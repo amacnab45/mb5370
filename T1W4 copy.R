72b234310b6adc9d11edb959282271691416e5b8
amacnab45 <alexander.macnab@my.jcu.edu.au>
2026-09-24 19:12:41 +1000

    Add T1W4 copy.R and module 3 work

---
+++ T1W4 copy.R
@@ -0,0 +1,13 @@
+#Set working directory
+setwd("Desktop/Master of Marine Biology/T1/")
+
+file.edit("/Desktop/Master of Marine Biology/T1/")
+setwd(path.expand("~/Desktop/Master of Marine Biology/T1"))
+setwd("/Users/alexandermacnab24/Desktop/Master of Marine Biology/T1")
+
+
+getwd()
+data <- read.csv("my_data.csv")
+file.edit("T1W4.R")
+# OR
+source("T1W4.R")
